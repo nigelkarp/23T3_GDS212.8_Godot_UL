df060c8b20ae7fbb75388fe5306e1dad227626e6
subway-aint-fresh <97781497+subway-aint-fresh@users.noreply.github.com>
2023-11-22 00:30:45 +1100

    Setting up cards for game

---
+++ scripts/Card.cs
@@ -0,0 +1,13 @@
+using Godot;
+using System;
+
+namespace Deck;
+
+class Card 
+{
+	[Export] private string suit;
+	[Export] private int value;
+	// [Export] variable for face
+	// variable for back 
+}
+
