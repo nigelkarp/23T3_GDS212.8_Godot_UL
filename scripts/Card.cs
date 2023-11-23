ab76b93a8a6da079e1c50eab09bb66e2017faf6a
subway-aint-fresh <97781497+subway-aint-fresh@users.noreply.github.com>
2023-11-23 19:27:23 +1100

    Setting up scene
    adding cards to the scene

--- scripts/Card.cs
+++ scripts/Card.cs
@@ -1,13 +1,5 @@
 using Godot;
 using System;
 
-namespace Deck;
 
-class Card 
-{
-	[Export] private string suit;
-	[Export] private int value;
-	// [Export] variable for face
-	// variable for back 
-}
 
